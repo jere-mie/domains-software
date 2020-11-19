965f7fd417fbd401bcfc0d31137c8c841bddbd75
jere-mie <jere-mie@users.noreply.github.com>
2020-11-19 13:52:36 -0500

    archived old legendre files

--- legendre.py
+++ legendre.py
@@ -1,31 +1,48 @@
 from matplotlib import pyplot as plt
-# import scipy.special.eval_legendre as lg
 from scipy.special import eval_legendre as lg
-# print(lg)
 from scipy.integrate import quad
 import numpy as np
 
-def integrand(x, l):
+
+# equation 23 in its 2 forms
+
+def integrand1(theta, l):
+    return np.power(((2.0*l + 1)/(4.0*np.pi)), 1.0/2.0)*lg(l, np.cos(theta))*np.sin(theta)
+
+def e23_1(l, ad):
+    return (2*np.pi)*(quad(integrand1, 0, ad, args=(l))[0])
+
+def integrand2(x, l):
     return lg(l, x)
 
-def element(l, ad):
-    return pow((np.pi*(2*l+1)), (1.0/2.0))*(quad(integrand, np.cos(ad), 1, args=(l))[0])
+def e23_2(l, ad):
+    return np.power((np.pi*(2*l+1)), (1.0/2.0))*(quad(integrand2, np.cos(ad), 1, args=(l))[0])
 
-def ele2(l, ad):
-    return (pow(element(l,ad), 2))*(pow(ad, -1))
+# equation 26 function
 
-# with open('w.txt', 'w') as f:
-#     for i in range(1, 11):
-#         f.write(str(element(i, np.pi/2.0))+"\n")
+def e26(l, ad):
+    a = (np.power(np.pi*(2.0*l + 1.0), (1.0/2.0)))/np.float(l)
+    b = np.cos(ad)*lg(int(l), np.cos(ad))
+    c = lg(int(l+1), np.cos(ad))
+    return a*(b-c)
+
+
+
+
+
+def y(l, alphad, ad):
+    # return (abs(e26(l, ad))**2)/float(ad)
+    # return (abs(e23_1(l, ad))**2)/float(ad)
+    return (abs(e23_2(l, alphad))**2)/ad
+
+def alpha(n, ad):
+    return (np.arccos(1-((2.0*ad)/n)))
 
 a = []
 b = []
 for i in range(1,11):
-    a.append(ele2(i, 0.025))
+    a.append(y(i, alpha(1, 0.1), 0.1))
     b.append(i)
 plt.plot(b, a)
 print(a)
 plt.savefig('lg.png')
-
-# def integrated(q,r1,r2,l):
-#   return (q**-3)*(quad(integrand,q*r1,q*r2, args=(l))[0])
--- test.py
+++
@@ -1,5 +0,0 @@
-from scipy.special import eval_legendre as lg
-from scipy.special import legendre as lg2
-# print(lg( 5, 2))
-# print(lg2(3))
-print(lg(3, 2))
