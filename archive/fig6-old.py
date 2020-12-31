6aff229f08f1bc9718a486272a93ea72b326fb7d
jere-mie <jere-mie@users.noreply.github.com>
2020-12-31 00:09:08 -0500

    fig6 now works in general

--- archive/fig6-old.py
+++ archive/fig6-old.py
@@ -24,7 +24,6 @@ angle = np.arccos(np.clip(c, -1, 1)) # if you really want the angle
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.special import eval_legendre as lg
-import re
 
 def e26(l, ad):
     a = (np.power(np.pi*(2.0*l + 1.0), (1.0/2.0)))/np.float(l)
@@ -45,35 +44,20 @@ def angle(a, b):
 
 l = []
 yaxis = []
-
 # ad = float(input("total domain area fraction: "))
-# ad = 0.05 times number of domains
-domains = int(input("number of domains: "))
-ad = 0.05*domains
-ealpha = alpha(domains, ad)
-vectors = []
-
-with open('fig6-in.txt', 'r') as f:
-    vect = []
-    for i in f:
-        vectors.append(re.findall("[\d.e-]+", i))
-        vectors[-1] = [float(num) for num in vectors[-1]]
-print(vectors)
+# TOTAL DOMAIN FRACTION
+ad = 0.1
 f = open('fig6.txt', 'w')
-
-for i in range(1, 15):
-    tot = 0
+# NUMBER OF DOMAINS IS IMPORTANT
+ealpha = alpha(2, ad)
+for i in range(1,15):
     l.append(i)
-    for j in range(domains):
-        for k in range(domains):
-            if j!=k:
-                element = e26(i, ealpha)
-                element = np.abs(element)**2
-                ang = angle(vectors[j], vectors[k])
-                element = element*lg(i, np.cos(ang))
-                tot+=element
-    yaxis.append(tot)
-    f.write(f'{tot}\n')
+    element = e26(i, ealpha)
+    element = np.abs(element)**2
+    element = element*lg(i, -1)
+    element*=2
+    yaxis.append(element)
+    f.write(f'{element}\n')
 f.close()
 plt.plot(l, yaxis)
 plt.savefig('fig6.png')
